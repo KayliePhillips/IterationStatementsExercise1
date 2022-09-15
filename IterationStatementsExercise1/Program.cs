9837d8855613b1c51a651083ee47bbc9d684bee8
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-14 19:47:53 -0500

    initial commit with additional practice

---
+++ IterationStatementsExercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write this for loop 15 times
+Console.WriteLine("-------- Exercise 1 -------------");
+
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+
+//Random Practice
+Console.WriteLine("-------- Random Practice for Better Understanding -------------");
+Console.WriteLine("-------- Write the loop 20 times, starting at 4. -------------");
+Console.WriteLine();
+for (int i = 4; i < 24; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+Console.WriteLine("-------- Write the loop 15 times, starting at 1. -------------");
+Console.WriteLine();
+for (int i = 1; i <=15; i++)
+{
+    Console.WriteLine($"{i}");
+}
